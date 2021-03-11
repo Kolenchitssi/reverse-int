4f1521c702c2d812e8dface3b0f24cd00448973a
Kolenchitssi <kolenchitssi@gmail.com>
2021-03-11 21:40:55 +0300

    feat: task resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = String(Math.abs(n));
+
+    let rev = str.split("").reverse().join("");
+    console.log(n, str, rev)
+    return rev;
 }
